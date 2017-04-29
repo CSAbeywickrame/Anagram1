fc4ce67650f41b2129e90b97bb90628621e4bcca
User <User@DESKTOP-HE4RHMT>
2017-04-29 22:14:43 +0530

    class manash created

---
+++ src/manash.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class manash {
+    
+    private int age;
+    private String name;
+        
+   public manash()
+   {
+       
+       
+   }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
